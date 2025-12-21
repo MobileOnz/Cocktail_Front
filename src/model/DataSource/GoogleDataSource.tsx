2f28e72e46bfa1e7b88c1ff27f19ba0546628bac
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2025-12-21 20:16:08 +0900

    lint 수정

--- src/model/DataSource/GoogleDataSource.tsx
+++ src/model/DataSource/GoogleDataSource.tsx
@@ -1,24 +1,24 @@
-import axios from "axios";
-import NaverLogin from "@react-native-seoul/naver-login";
-import { ISocialAuthDataSource } from "./ISocialAuthDataSource";
-import { AuthResult } from "../domain/AuthResult";
-import { AuthError, AuthErrorType } from "../domain/AuthError";
-import { API_BASE_URL } from "@env";
+import axios from 'axios';
+// import NaverLogin from "@react-native-seoul/naver-login";
+import { ISocialAuthDataSource } from './ISocialAuthDataSource';
+import { AuthResult } from '../domain/AuthResult';
+import { AuthError, AuthErrorType } from '../domain/AuthError';
+import { API_BASE_URL } from '@env';
 
 export class GoogleAuthDataSource implements ISocialAuthDataSource {
 
-  async getLoginUrl(): Promise< {loginUrl: string}> {
+  async getLoginUrl(): Promise<{ loginUrl: string }> {
     const response = await axios.get(
       `${API_BASE_URL}/api/v2/auth/google/login-url`
     );
-    console.log("getLoginUrl: ", response.data)
-    return response.data
+    console.log('getLoginUrl: ', response.data);
+    return response.data;
   }
 
   async login(): Promise<AuthResult> {
     throw new AuthError(
       AuthErrorType.SERVER_ERROR,
-      "서버 오류"
+      '서버 오류'
     );
   }
 }
