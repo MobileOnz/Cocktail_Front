f1cab6269543a2f5e6b2b0a8addb00f3b672deb2
yunsuk990 <yunsuk990@naver.com>
2025-12-27 21:19:51 +0900

    Update RecommendationViewModel.tsx

--- src/Screens/Recommend/RecommendationViewModel.tsx
+++ src/Screens/Recommend/RecommendationViewModel.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 // import { CocktailRecommendRepository } from "../../model/Repository/CocktailRecommendRepository";
 // import { RecommendCocktailDataSource } from "../../model/DataSource/RecommendCocktailDataSource";
 
